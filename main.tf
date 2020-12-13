6bde4da7dd2f96643e9e0fc933b81e4070af6d9e
Jitendra Singh <dba.jitendra@gmail.com>
2020-12-13 12:10:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,13 @@ resource "aws_backup_vault" "testvault" {
   name        = var.VAULT_NAME
   }
 
+
 resource "aws_backup_plan" "plan1" {
   name = var.Plan_Name
 
   rule {
     rule_name         = var.Rule_Name
-    target_vault_name = var.VAULT_NAME
+    target_vault_name = aws_backup_vault.testvault.name
     schedule          = "cron(0 12 * * ? *)"
   }
 
